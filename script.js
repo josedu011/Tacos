772defbe8dc7f43bb126fecae4234c79221afaaa
josedu011 <65740596+josedu011@users.noreply.github.com>
2020-08-05 14:00:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 var btnno = document.getElementById('btnno');
 var btnyes = document.getElementById('btnyes');
 
-btnno.addEventListener('mouseenter', () => {
-    console.log("mouse entrando");
-    var pos = coordenadas();
-    btnno.style.top = `${pos.posy}px`;
-    btnno.style.left = `${pos.posx}px`;
-})
+btnno.addEventListener('click', () => {
+    alert('De lo que te pierdes');
+});
 
-var coordenadas = () => {
-    var ancho = screen.width - 100;
-    var alto = screen.height - 200;
-    var posx = Math.random() * (ancho - 0) + 0;
-    var posy = Math.random() * (alto - 0) + 0;
-    return { posx, posy }
-}
 
 btnyes.addEventListener('click', () => {
-    alert('Que bueno porque estas a dieta');
+    alert('Te veo después de la cuarentena');
 });
